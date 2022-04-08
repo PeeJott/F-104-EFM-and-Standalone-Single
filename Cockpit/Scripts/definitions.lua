14816831bc7b306a9c1555563985b37859ced7ae
Copprhead <stefan.rothdach@googlemail.com>
2022-04-08 22:39:26 +0200

    Temporary fix for station selection Moved "dofile" for radar to correct line

---
+++ Cockpit/Scripts/definitions.lua
@@ -0,0 +1,17 @@
+
+
+-- global defintions
+
+
+PYLON = 	{
+				-- Name to station number
+				LH_TIP = 1,
+				-- 104S = 2
+				LH_PYLON = 3,				
+				LH_FUS = 4,				
+				--CENTERLINE = 5,
+				RH_FUS = 6,
+				RH_PYLON = 7,
+				-- 104S = 8
+				RH_TIP = 9,				
+}
